1b3a689ff2143c67819c42de13fe0fa5ff219304
Chambers <964089@swansea.ac.uk>
2021-02-07 15:14:15 +0000

    Wheelchair Scripts

---
+++ Assets/WheelChairMove.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WheelChairMove : MonoBehaviour
+{
+    public float turnAmount = 10f;
+    public float torque = 10f;
+    public float thrust = 1.0f;
+    public Rigidbody rb;
+    public WheelChairWheel rightWheel;
+    public WheelChairWheel leftWheel;
+
+
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void FixedUpdate()
+    {
+        // rb.AddForce(transform.forward * thrust);
+        // Short hand version of == true 
+
+        if (rightWheel.beingPushed && leftWheel.beingPushed)
+        {
+            rb.AddForce(transform.forward * thrust);
+        }
+        else
+        {
+            if (rightWheel.beingPushed == true)
+            {
+                rb.AddTorque(transform.up * torque * -turnAmount);
+            }
+
+            if (leftWheel.beingPushed == true)
+            {
+                rb.AddTorque(transform.up * torque * turnAmount);
+
+            }
+        }
+    }
+}
+
---
+++ Assets/WheelChairWheel.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WheelChairWheel : MonoBehaviour
+{
+    public bool beingPushed;
+    public enum WheelType
+    {
+        Left,
+        Right,
+    }
+
+    public WheelType wheelType;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (wheelType == WheelType.Left)
+        {
+
+            if (Input.GetKey(KeyCode.Q))
+            {
+                beingPushed = true;
+            }
+            else
+            {
+                beingPushed = false;
+            }
+        }
+
+        if (wheelType == WheelType.Right)
+        {
+
+            if (Input.GetKey(KeyCode.E))
+            {
+                beingPushed = true;
+            }
+            else
+            {
+                beingPushed = false;
+            }
+        }
+
+    }
+}
+
