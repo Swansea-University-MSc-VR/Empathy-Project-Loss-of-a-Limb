290a77cb8d14573c66ab5d0a12dfb2a2fb03905e
2030428 <72548933+2030428@users.noreply.github.com>
2021-02-12 12:02:34 +0000

    Created main scene and began asset migration - scene controller script added

---
+++ Assets/Scripts/SceneController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Video;
+
+public class SceneController : MonoBehaviour
+{
+    public GameObject wheelChair, environment, playerModel, videoSphere;          //creates refeences for gameobjects
+    public VideoPlayer VRVideo;                             //creates references for video player
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        wheelChair.SetActive(false);                        //ensures gameobject is inactive
+        environment.SetActive(false);                           //ensures gameobject is inactive
+        playerModel.SetActive(false);                            //ensures gameobject is inactive
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (VRVideo.isPlaying != true)                      //if the video has finished playing....
+        {
+            videoSphere.SetActive(false);                   //deactivates gameobject
+            wheelChair.SetActive(true);                     //activate gameobject
+            environment.SetActive(true);                    //activate gameobject
+            playerModel.SetActive(true);                    //activate gameobject
+        }
+    }
+}
