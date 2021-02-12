2ffbf4b94c637b7a86282d3212cc9cd984e6edc7
Lemlurker <72548980+Lemlurker@users.noreply.github.com>
2021-02-12 15:23:11 +0000

    added alt control scheme "turn on enter" without velocity tracking

---
+++ Assets/TurnOnEnter.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TurnOnEnter : MonoBehaviour
+{
+    public Rigidbody rb;
+    public float thrust = 40f;
+    public float torque = 40f;
+    public float LinkHandVel;
+    public GameObject HandTrack;
+    
+  
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        
+
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        rb.AddTorque(transform.up * torque );//* LinkHandVel
+        rb.AddForce(transform.forward * thrust ); // * LinkHandVel// 
+        LinkHandVel = HandTrack.GetComponent<velocityTrack>().velocityClamp;
+
+
+    }
+
+   
+}
+
+//rb.AddTorque(transform.up * torque * turnAmount);
---
+++ Assets/velocityTrack.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class velocityTrack : MonoBehaviour
+{
+    public float CurrentYPos;
+    public float PreviousYPos;
+    public float velocity;
+    public float velocityClamp;
+    public float velocityModifier = 10;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        CurrentYPos = transform.position.y;
+        velocity = ((CurrentYPos - PreviousYPos) / Time.deltaTime) ;
+        velocityClamp = Mathf.Clamp(velocity, 0.0f, 2.0f) * velocityModifier;
+        PreviousYPos = CurrentYPos;
+    }
+}
