384341e293ae0a0f9220017c71deebd05607b56a
Lemlurker <72548980+Lemlurker@users.noreply.github.com>
2021-02-12 17:36:44 +0000

    grip experimentation

--- Assets/ObjectActiveOnCollission.cs
+++ Assets/ObjectActiveOnCollission.cs
@@ -1,12 +1,23 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class ObjectActiveOnCollission : MonoBehaviour
 {
     
     public GameObject Turn;
+    public InputActionReference XRIntMangr;
 
+    private void OnEnable()
+    {
+        XRIntMangr.action.performed += GripPressed;
+    }
+
+    private void GripPressed(InputAction.CallbackContext obj)
+    {
+        
+    }
 
     // Start is called before the first frame update
     void Start()
@@ -21,12 +32,15 @@ public class ObjectActiveOnCollission : MonoBehaviour
 
     void OnTriggerEnter(Collider other)
     {
-        if (other.gameObject.tag == "Hand")
+        if (other.gameObject.tag == "Hand"  )
         {
-           
-            Turn.SetActive(true);
+           if(XRIntMangr.action.triggered)
+            {
+                Turn.SetActive(true);
+            }
+            //Turn.SetActive(true);
 
-            Debug.Log("Collided");
+            //Debug.Log("Collided");
         }
     }
     void OnTriggerExit(Collider other)
@@ -35,19 +49,16 @@ public class ObjectActiveOnCollission : MonoBehaviour
         {
             
             Turn.SetActive(false);
-            Debug.Log("stopped Colliding");
+            //Debug.Log("stopped Colliding");
+       }
+   }
+    // Update is called once per frame
+   void Update()
+    {
+        if(XRIntMangr.action.triggered)
+        {
+            Debug.Log("Triggered");
+            //Turn.SetActive(true);
         }
     }
-    // Update is called once per frame
-   // void Update()
-   // {
-      //  if(Input.GetKey(KeyCode.W))
-      //  {
-     //       Push.SetActive(true);
-      //  }
-      //  else
-      //  {
-     //       Push.SetActive(false);
-      //  }
-    //}
 }
