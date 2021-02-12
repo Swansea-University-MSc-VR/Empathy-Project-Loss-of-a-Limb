558fca2ec2683e40d67f3950ea90e694743cfa3f
2030428 <72548933+2030428@users.noreply.github.com>
2021-02-12 12:21:42 +0000

    editted video applied to sphere - scene controller comments added

--- Assets/Scripts/SceneController.cs
+++ Assets/Scripts/SceneController.cs
@@ -5,27 +5,32 @@ using UnityEngine.Video;
 
 public class SceneController : MonoBehaviour
 {
-    public GameObject wheelChair, environment, playerModel, videoSphere;          //creates refeences for gameobjects
-    public VideoPlayer VRVideo;                             //creates references for video player
+    public GameObject wheelChair, environment, playerModel, videoSphere;            //creates refeences for gameobjects
+    public VideoPlayer VRVideo;                                                     //creates references for video player
 
 
     // Start is called before the first frame update
     void Start()
     {
-        wheelChair.SetActive(false);                        //ensures gameobject is inactive
-        environment.SetActive(false);                           //ensures gameobject is inactive
-        playerModel.SetActive(false);                            //ensures gameobject is inactive
+        wheelChair.SetActive(false);                     //ensures gameobject is inactive
+        environment.SetActive(false);                    //ensures gameobject is inactive
+        playerModel.SetActive(false);                    //ensures gameobject is inactive
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (VRVideo.isPlaying != true)                      //if the video has finished playing....
+        if (VRVideo.isPlaying != true)                  //if the video has finished playing....
         {
-            videoSphere.SetActive(false);                   //deactivates gameobject
-            wheelChair.SetActive(true);                     //activate gameobject
-            environment.SetActive(true);                    //activate gameobject
-            playerModel.SetActive(true);                    //activate gameobject
+            LaunchEnvironment();                        //call funstion
         }
+    }
+
+    void LaunchEnvironment()
+    {
+        videoSphere.SetActive(false);                   //deactivates gameobject
+        wheelChair.SetActive(true);                     //activate gameobject
+        environment.SetActive(true);                    //activate gameobject
+        playerModel.SetActive(true);                    //activate gameobject
     }
 }
