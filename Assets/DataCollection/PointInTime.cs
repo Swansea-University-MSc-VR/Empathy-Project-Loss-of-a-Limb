c1da1b1def895fdf1b94d3351d127570b63ce531
Chambers <964089@swansea.ac.uk>
2021-02-14 23:46:41 +0000

    Added data collection for the HMD based on Marcs script and lecture.

---
+++ Assets/DataCollection/PointInTime.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class PointInTime
+
+{
+	public float time;
+	public Vector3 position;
+	public Quaternion rotation;
+
+	public PointInTime(float _time, Vector3 _position, Quaternion _rotation)
+    {
+		time = _time;
+		position = _position;
+		rotation = _rotation; 
+    }
+
+
+}
