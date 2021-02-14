69b9ec5e50b9b9e1693e901d61e6af9c7f19a7a2
Lemlurker <72548980+Lemlurker@users.noreply.github.com>
2021-02-14 21:22:40 +0000

    Added potential wheelchjair locomotion, requires assesment

---
+++ Assets/comset.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class comset : MonoBehaviour
+{
+    public Vector3 com;
+    public Rigidbody rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb.centerOfMass = com;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
