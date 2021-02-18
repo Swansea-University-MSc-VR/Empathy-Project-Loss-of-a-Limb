4e2ca07e59f2f7c692df61b38fa755a3dfbb841f
Lemlurker <72548980+Lemlurker@users.noreply.github.com>
2021-02-18 11:19:41 +0000

    Update Recorder.cs

--- Assets/Scripts/Recorder.cs
+++ Assets/Scripts/Recorder.cs
@@ -4,12 +4,6 @@ using UnityEngine;
 
 public class Recorder : MonoBehaviour
 {
-  //  public Survey surveyData;
-  //  public Survey2 survey2Data;
-  //  public SceneController saveTime;
-
-    public int[] surveyAnswers;
-
     [System.Serializable]
     public class WrappingClassTrack
     {
@@ -30,24 +24,10 @@ public class Recorder : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (save) //Time.save)           //change to if (saveTime) to active from SceneController
+        if (save )
         {
             if (!saveOnce)
             {
-               /* surveyAnswers[0] = surveyData.answers[0];
-                surveyAnswers[1] = surveyData.answers[1];
-                surveyAnswers[2] = surveyData.answers[2];
-                surveyAnswers[3] = surveyData.answers[3];
-                surveyAnswers[4] = surveyData.answers[4];
-                surveyAnswers[5] = surveyData.answers[5];
-                surveyAnswers[6] = surveyData.answers[6];
-                surveyAnswers[7] = surveyData.answers[7];
-                surveyAnswers[8] = survey2Data.answers2[0];
-                surveyAnswers[9] = survey2Data.answers2[1];
-                surveyAnswers[10] = survey2Data.answers2[2];
-                surveyAnswers[11] = survey2Data.answers2[3]; */
-
-
                 var variable = new WrappingClassTrack() { TrackMainCamera = Track };
                 string debugTrack = JsonUtility.ToJson(variable);
                 Debug.Log(debugTrack);
