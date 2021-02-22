799ade2e9248ce02d6fca3362695ca3d890dac56
2030428 <72548933+2030428@users.noreply.github.com>
2021-02-22 16:24:49 +0000

    amended canvases for XR rig - updated event system for XR

--- Assets/Scripts/SceneController.cs
+++ Assets/Scripts/SceneController.cs
@@ -9,7 +9,7 @@ public class SceneController : MonoBehaviour
 {
     public Survey survey2Finished;                                                 //reference to surveyController script
 
-    public GameObject wheelChair, environment, playerModel, video1Sphere, video2Sphere, survey1Canvas, survey2Canvas, welcomeCanvas, finishCanvas;      //creates refeences for gameobjects
+    public GameObject XRRig, wheelChair, environment, video1Sphere, video2Sphere, survey1Canvas, survey2Canvas, welcomeCanvas, finishCanvas;      //creates refeences for gameobjects
     public VideoPlayer VRVideo1, VRVideo2;                                                  //creates references for video player
     public bool survey1Complete, survey2Complete, environmentComplete, restart, close;                                           //bools to control
     public Button next, replay, finish;
@@ -23,11 +23,12 @@ public class SceneController : MonoBehaviour
         video2Sphere.SetActive(false);                           //ensures game object inactive
         wheelChair.SetActive(false);                            //ensures gameobject is inactive
         environment.SetActive(false);                           //ensures gameobject is inactive
-        playerModel.SetActive(false);                           //ensures gameobject is inactive
         welcomeCanvas.SetActive(true);
         survey1Canvas.SetActive(false);                          //sets this game object to active
         survey2Canvas.SetActive(false);                         //sets this game object to inactive
         finishCanvas.SetActive(false);
+        XRRig.SetActive(true);
+
 
     }
 
@@ -68,6 +69,8 @@ public class SceneController : MonoBehaviour
     {
         welcomeCanvas.SetActive(false);
         survey1Canvas.SetActive(true);
+        XRRig.SetActive(true);
+
     }
 
     public void LaunchVideo1()
@@ -76,6 +79,8 @@ public class SceneController : MonoBehaviour
         VRVideo1.Play();                                        //play video file
         survey1Canvas.SetActive(false);                          //turn off survey canvas
         survey1Complete = true;                                 //set bool to true
+        XRRig.SetActive(true);
+
     }
 
     void LaunchEnvironment()
@@ -83,15 +88,17 @@ public class SceneController : MonoBehaviour
         video1Sphere.SetActive(false);                   //deactivates gameobject
         wheelChair.SetActive(true);                     //activate gameobject
         environment.SetActive(true);                    //activate gameobject
-        playerModel.SetActive(true);                    //activate gameobject
+        XRRig.SetActive(false);
+
     }
 
     void LaunchSurvey2()
     {
         wheelChair.SetActive(false);                    //ensures gameobject is inactive
         environment.SetActive(false);                   //ensures gameobject is inactive
-        playerModel.SetActive(false);                   //ensures gameobject is inactive
         survey2Canvas.SetActive(true);                   //sets this game object to active
+        XRRig.SetActive(true);
+
     }
 
     public void LaunchVideo2()
@@ -100,6 +107,8 @@ public class SceneController : MonoBehaviour
         video2Sphere.SetActive(true);                    //set gameobject to active
         VRVideo2.Play();                                //play video file
         survey2Complete = true;
+        XRRig.SetActive(true);
+
 
     }
 
@@ -107,6 +116,8 @@ public class SceneController : MonoBehaviour
     {
         video2Sphere.SetActive(false);
         finishCanvas.SetActive(true);
+        XRRig.SetActive(true);
+
     }
 
     void RestartBtn()
