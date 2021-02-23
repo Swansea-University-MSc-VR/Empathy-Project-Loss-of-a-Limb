6e59d9da356d8c7cee357e00c1388dc0f3f97106
2030428 <72548933+2030428@users.noreply.github.com>
2021-02-23 13:46:32 +0000

    amended lightbake in current main scene - final adjust lighting settings - commenting on scene controller script completed

--- Assets/Scripts/SceneController.cs
+++ Assets/Scripts/SceneController.cs
@@ -7,131 +7,130 @@ using UnityEngine.SceneManagement;
 
 public class SceneController : MonoBehaviour
 {
-    public Survey survey2Finished;                                                 //reference to surveyController script
+    public Survey survey2Finished;                                                                                                                          //reference to surveyController script
 
-    public GameObject XRRig, wheelChair, environment, video1Sphere, video2Sphere, survey1Canvas, survey2Canvas, welcomeCanvas, finishCanvas;      //creates refeences for gameobjects
-    public VideoPlayer VRVideo1, VRVideo2;                                                  //creates references for video player
-    public bool survey1Complete, survey2Complete, environmentComplete, restart, close;                                           //bools to control
+    public GameObject XRRig, XRRig3, wheelChair, environment, video1Sphere, video2Sphere, survey1Canvas, survey2Canvas, welcomeCanvas, finishCanvas;        //creates refeences for gameobjects
+    public VideoPlayer VRVideo1, VRVideo2;                                                                                                                  //creates references for video player
+    public bool survey1Complete, survey2Complete, environmentComplete, restart, close, Vid2Played;                                                          //bools to control
     public Button next, replay, finish;
 
 
     void Start()
     {
-        VRVideo1.Stop();                                        //ensures video not playing
-        VRVideo2.Stop();                                        //ensures video is not playing
-        video1Sphere.SetActive(false);                           //ensures game object inactive
-        video2Sphere.SetActive(false);                           //ensures game object inactive
-        wheelChair.SetActive(false);                            //ensures gameobject is inactive
-        environment.SetActive(false);                           //ensures gameobject is inactive
-        welcomeCanvas.SetActive(true);
-        survey1Canvas.SetActive(false);                          //sets this game object to active
-        survey2Canvas.SetActive(false);                         //sets this game object to inactive
-        finishCanvas.SetActive(false);
-        XRRig.SetActive(true);
-
-
+        VRVideo1.Stop();                                //ensures video not playing
+        VRVideo2.Stop();                                //ensures video is not playing
+        video1Sphere.SetActive(false);                  //ensures game object inactive
+        video2Sphere.SetActive(false);                  //ensures game object inactive
+        wheelChair.SetActive(false);                    //ensures gameobject is inactive
+        environment.SetActive(false);                   //ensures gameobject is inactive
+        welcomeCanvas.SetActive(true);                  //sets this game object to active
+        survey1Canvas.SetActive(false);                 //sets this game object to active
+        survey2Canvas.SetActive(false);                 //sets this game object to inactive
+        finishCanvas.SetActive(false);                  //sets this game object to inactive
+        XRRig.SetActive(true);                          //sets this game object to active
+        Vid2Played = false;                             //sets bool false
     }
 
     // Update is called once per frame
     void Update()
-    {
-       
-
+    {       
         next.onClick.AddListener(LaunchSurvey1);
         replay.onClick.AddListener(RestartBtn);
         finish.onClick.AddListener(Finish);
 
         if (VRVideo1.isPlaying != true && survey1Complete == true && survey2Complete == false)      //if the video has finished playing and survey is completed....
         {
-            LaunchEnvironment();                                        //call funstion
+            LaunchEnvironment();                                                                    //call funstion
         }
-        if (environmentComplete && survey2Complete == false)
+        if (environmentComplete && survey2Finished.postSurveyComplete == false)                     //if this condition is met....
         {
-            LaunchSurvey2();
+            LaunchSurvey2();                                                                        //call this function
         }
-        if(VRVideo2.isPlaying != true && survey2Finished.postSurveyComplete)
+        if(environmentComplete && survey2Finished.postSurveyComplete)                               //if this condition is met....
         {
-            LaunchFinishCanvas();
+            LaunchVideo2();                                                                         //call this function
         }
-        if (restart)
+        if(VRVideo2.isPlaying != true && survey2Finished.postSurveyComplete && Vid2Played)          //if this condition is met....
         {
-            Replay();
+            LaunchFinishCanvas();                                                                   //call this function
         }
-        if (close)
+        if (restart)                                                                                //uf this condition is met.....
         {
-            //UnityEditor.EditorApplication.isPlaying = false;
-            Application.Quit();
+            Replay();                                                                               //call this function
+        }
+        if (close)                                                                                  //if this condition is met....
+        {
+            //UnityEditor.EditorApplication.isPlaying = false;                                      //stop editor playback
+            Application.Quit();                                                                     //close application
         }
 
     }
 
     void LaunchSurvey1()
     {
-        welcomeCanvas.SetActive(false);
-        survey1Canvas.SetActive(true);
-        XRRig.SetActive(true);
-
+        welcomeCanvas.SetActive(false);                     //turns off GO
+        survey1Canvas.SetActive(true);                      //turns on GO
+        XRRig.SetActive(true);                              //turns on GO
     }
 
     public void LaunchVideo1()
     {
-        video1Sphere.SetActive(true);                            //set gameobject to active
-        VRVideo1.Play();                                        //play video file
-        survey1Canvas.SetActive(false);                          //turn off survey canvas
-        survey1Complete = true;                                 //set bool to true
-        XRRig.SetActive(true);
+        video1Sphere.SetActive(true);                       //set gameobject to active
+        VRVideo1.Play();                                    //play video file
+        survey1Canvas.SetActive(false);                     //turn off survey canvas
+        survey1Complete = true;                             //set bool to true
+        XRRig.SetActive(true);                              //turns on GO
 
     }
 
     void LaunchEnvironment()
     {
-        video1Sphere.SetActive(false);                   //deactivates gameobject
-        wheelChair.SetActive(true);                     //activate gameobject
-        environment.SetActive(true);                    //activate gameobject
-        XRRig.SetActive(false);
+        video1Sphere.SetActive(false);                      //deactivates gameobject
+        wheelChair.SetActive(true);                         //activate gameobject
+        environment.SetActive(true);                        //activate gameobject
+        XRRig.SetActive(false);                             //turns off GO
 
     }
 
     void LaunchSurvey2()
     {
-        wheelChair.SetActive(false);                    //ensures gameobject is inactive
-        environment.SetActive(false);                   //ensures gameobject is inactive
-        survey2Canvas.SetActive(true);                   //sets this game object to active
-        XRRig.SetActive(true);
+        wheelChair.SetActive(false);                        //ensures gameobject is inactive
+        environment.SetActive(false);                       //ensures gameobject is inactive
+        survey2Canvas.SetActive(true);                      //sets this game object to active
+        XRRig3.SetActive(true);                             //turns on GO
 
     }
 
     public void LaunchVideo2()
     {
-        survey2Canvas.SetActive(false);                  //turn off survey canvas
-        video2Sphere.SetActive(true);                    //set gameobject to active
-        VRVideo2.Play();                                //play video file
-        survey2Complete = true;
-        XRRig.SetActive(true);
-
-
+        survey2Canvas.SetActive(false);                     //turn off survey canvas
+        video2Sphere.SetActive(true);                       //set gameobject to active
+        VRVideo2.Play();                                    //play video file
+        survey2Complete = true;                             //sets bool true
+        XRRig3.SetActive(true);                             //turns on GO
+        Vid2Played = true;                                  //sets bool true
     }
 
     void LaunchFinishCanvas()
     {
-        video2Sphere.SetActive(false);
-        finishCanvas.SetActive(true);
-        XRRig.SetActive(true);
-
+        video2Sphere.SetActive(false);                      //turns off GO
+        finishCanvas.SetActive(true);                       //turns on GO
+        XRRig3.SetActive(true);                             //turns on GO
+                
     }
 
     void RestartBtn()
     {
-        restart = true;
+        restart = true;                                     //sets bool true
     }
     
     void Replay()
     {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);       //reloads currently loaded scene
     }
 
     void Finish()
     {
-        close = true;
+        close = true;                                       //sets bool true
     }
 }
